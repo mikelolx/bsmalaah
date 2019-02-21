7a85d0ef28260d0d88a4a90b7c2f2867deb044db
mikelolx <47793088+mikelolx@users.noreply.github.com>
2019-02-21 05:55:32 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const canvas = require('canvas');
 const jimp = require('jimp');
-const prefix = '*';
+const prefix = '=';
 const axios = require('axios');
 const fs = require('fs');
 const ms = require('ms');
